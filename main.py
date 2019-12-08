6990dabe18f90ba600c7ec7d487920986d33f559
sstarnovskii <kopernik.t.800@gmail.com>
2019-12-08 19:23:41 +0700

    input.txt added, todos added.

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ()
+
+# TODO: make function to read input.txt
+
+# TODO: make function to import data from links.
+
+# TODO: make function to write data into output.txt
